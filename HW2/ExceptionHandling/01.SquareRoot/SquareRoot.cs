b9350ee94ef653bf761f47abe03caa6cb591d29f
IvoStoianov <ivostoianov94@gmail.com>
2015-01-26 15:28:02 +0200

    Completed HW 2.2

---
+++ HW2/ExceptionHandling/01.SquareRoot/SquareRoot.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace _01.SquareRoot
+{
+    class SquareRoot
+    {
+        static void Main()
+        {
+            try
+            {
+                int number = int.Parse(Console.ReadLine());
+                if(number<0)
+                    throw new ArgumentOutOfRangeException();
+                else
+                    Console.WriteLine(Math.Sqrt(number));
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Invalid Number");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid Number");
+            }
+            finally
+            {
+                Console.WriteLine("Good bye!");
+            }
+        }
+    }
+}
---
+++ HW2/ExceptionHandling/02.EnterNumbers/EnterNumbers.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace _02.EnterNumbers
+{
+    class EnterNumbers
+    {
+        public static int ReadNumber(int start, int end)
+        {
+            int number = 0;
+            try
+            {
+                number = int.Parse(Console.ReadLine());
+                if (number < start || number > end)
+                    throw new ArgumentOutOfRangeException();
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("The number that you entered is < than {0} or > then {1}", start, end);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid number");
+            }
+            return number;
+        }
+        static void Main()
+        {
+            int start = 1;
+            int[] a = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Please enter number {0}: ", i+1);
+                a[i] = ReadNumber(start, 100);
+                while (a[i] <= start || a[i] >= 100)
+                {
+                    Console.WriteLine("Please enter another number {0}: ", i+1);
+                    a[i] = ReadNumber(start, 100);
+                }
+                start = a[i];
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(a[i]);
+            }
+        }
+    }
+}
